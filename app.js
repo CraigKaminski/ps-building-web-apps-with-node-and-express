2fa5295a84dd47f40196c2bcebd3a12b75b6db28
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-06-23 15:01:24 -0400

    Install ejs pacakge and set as view engine

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
