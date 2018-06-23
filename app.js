c6ab2d698422a849e693297eddc8ba0848f174a6
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-06-23 15:02:38 -0400

    Add bootstrap theme

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.render('index', { list: ['a', 'b'], title: 'Library' });
 });
 
 app.listen(port, () => {
